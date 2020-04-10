e541f7f77918d725c07e22de26a4e17effd2651d
Steven Angel Coaila Zaa <51928039+StevenACZ@users.noreply.github.com>
2020-04-10 18:37:58 -0500

    rediseñando la interfas

--- src/js/app.js
+++ src/js/app.js
@@ -17,16 +17,46 @@ let vue = new Vue({
         total_cases,
         total_recovered,
         total_deaths,
-        total_active_cases
+        total_active_cases,
+        total_serious_cases,
+        total_new_deaths_today,
+        total_new_cases_today,
       } = cases;
 
       const card = document.createElement('article');
       card.classList.add('card')
       card.innerHTML = `
-        <h1 class="card__title">Total de casos: ${total_cases}</h1>
-        <p>Fallecidos: ${total_deaths}</p>
-        <p>Recuperados: ${total_recovered}</p>
-        <p>Casos activos: ${total_active_cases}</p>
+        <div class="subCard">
+          <h2>${total_cases}</h2>
+          <h1 class="card__title">Total de casos</h1>
+        </div>
+
+        <section class="subCard__Container">
+          <div class="subCard">
+            <p>${total_deaths}</p>
+            <p class="subCard__subTitle">Fallecidos</p>
+          </div>
+          <div class="subCard">
+            <p>${total_recovered}</p>
+            <p class="subCard__subTitle">Recuperados</p>
+          </div>
+          <div class="subCard">
+            <p>${total_active_cases}</p>
+            <p class="subCard__subTitle">Casos activos</p>
+          </div>
+          <div class="subCard">
+            <p>${total_new_cases_today}</p>
+            <p class="subCard__subTitle">Nuevos casos</p>
+          </div>
+          <div class="subCard">
+            <p>${total_new_deaths_today}</p>
+            <p class="subCard__subTitle">Nuevos fallecidos</p>
+          </div>
+          <div class="subCard">
+            <p>${total_serious_cases}</p>
+            <p class="subCard__subTitle">Casos delicados</p>
+          </div>
+        </section>
         `;
         
       return card;
